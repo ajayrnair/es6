26208228966e1089149f11f5f43b789a8f31c052
Ajay Ravindran Nair <ajayn@fb.com>
2018-12-23 19:57:27 +0000

    moving files to correct folders

--- functions/1.functions.js
+++ functions/1.functions.js
@@ -62,28 +62,3 @@ noNewUsed(); // undefined
 function newUsed() {
     console.log(new.target);
 }
-
-/*
- * Arrow functions borrow 'this' from the containing parent (global if arrow function is not contained)
- * Cannot change "this" using bind either
- * Cannot be called using new
- * No new.target, super, arguments
- * When not to use arrow function?
- *  - within object literal, the property function should not be arrow since 'this' will be containing parent
- */
-
- // property function in an object literal
- const object1 = {
-    prop1: 1,
-    method1: () => {
-        console.log(this); // window object
-    }
- };
-
- // instead
- const object2 = {
-     prop1: 1,
-     method1() {
-         console.log(this); // object2
-     }
- }
